79ff5a1b49e56fb38b6c7cffbc83d9a123249d37
Pablo Gramajo <pgramajo@jbknowledge.com>
2018-06-11 14:46:44 -0300

    first commit

---
+++ src/reducer/index.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux';
+// import peopleReducer from '../people/peopleReducer';
+// import usuariosReducer from '../usuarios/usuariosReducer';
+// import aboutReducer from '../about/aboutReducer';
+// import { reducer as reduxFormReducer } from 'redux-form';
+
+const rootReducer = combineReducers({
+//    peopleReducer ,
+//    usuariosReducer ,
+//    aboutReducer,
+//    form: reduxFormReducer
+});
+export default rootReducer ;
